996ca2f50840f04c624ae54fff83e821f508eea9
tylerolson3 <67395239+tylerolson3@users.noreply.github.com>
2021-10-10 17:15:04 -0500

    added error handling to input field

--- src/app.js
+++ src/app.js
@@ -17,20 +17,16 @@ let marker = '';
 async function getIP() {
     let res = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${key}&ipAddress`)
     let data = await res.json()
-    console.log(data)
+    console.log(data.location)
     let {ip, isp} = data
-    let {city, postalCode, lat, lng, timezone} = data.location;
+    let {city, postalCode, lat, lng, timezone, region} = data.location;
     console.log('ip:',ip)
-    console.log('isp:', isp)
     console.log('city:', city)
-    console.log('postal code:', postalCode)
-    console.log('lat/long:', lat, lng)
-    console.log('timezone:', timezone)
 
     displayIP.textContent = ip
     displayISP.textContent = isp
-    displayCity.textContent = city + "" + postalCode
-    displayTimezone.textContent = timezone
+    displayCity.textContent = `${city}, ${region} ${postalCode}`
+    displayTimezone.textContent = `UTC ${timezone}`
 
     mymap = L.map('mapid').setView([lat, lng], 13);
     marker = L.marker([lat, lng]).addTo(mymap);
@@ -41,28 +37,24 @@ L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
+    accessToken: 'your.mapbox.access.token',
 }).addTo(mymap);
-
+    mymap.zoomControl.remove();
 }
 
-async function updateIp() {
-let q = '172.93.177.85'
-    let res = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${key}&ipAddress=172.93.177.85`)
+async function updateIp(newIpAddress) {
+let q = newIpAddress
+    let res = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${key}&ipAddress=${q}`)
     let data = await res.json()
     let {ip, isp} = data
-    let {city, postalCode, lat, lng, timezone} = data.location;
+    let {city, postalCode, lat, lng, timezone, region} = data.location;
     console.log('ip:',ip)
-    console.log('isp:', isp)
     console.log('city:', city)
-    console.log('postal code:', postalCode)
-    console.log('lat/long:', lat, lng)
-    console.log('timezone:', timezone)
 
     displayIP.textContent = ip
     displayISP.textContent = isp
-    displayCity.textContent = city + "" + postalCode
-    displayTimezone.textContent = timezone
+    displayCity.textContent = `${city}, ${region} ${postalCode}`
+    displayTimezone.textContent = `UTC ${timezone}`
 
     console.log(mymap)
 
@@ -77,7 +69,15 @@ let q = '172.93.177.85'
 
 function onClick(e) {
     console.log('clicked!!!', searchInput.value)
-    updateIp()
+    let sampleIp = searchInput.value
+    let re = new RegExp('^([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$')
+    let result = re.test(sampleIp)
+    
+    result ? updateIp(searchInput.value) : alert('please enter a valid IP adress')
+
+    searchInput.value = ''
+
+    
 }
 
 searchBtn.addEventListener('click', onClick)
